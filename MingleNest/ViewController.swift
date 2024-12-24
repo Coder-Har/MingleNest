95f2f96972d522102505faca198212374e065d53
Coder-Har <harshitsharma12250@gmail.com>
2024-12-25 00:52:09 +0530

    Initial Commit

---
+++ MingleNest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MingleNest
+//
+//  Created by Harshit Sharma on 25/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hey")
+    }
+
+
+}
+
